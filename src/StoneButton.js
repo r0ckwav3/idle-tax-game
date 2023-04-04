c9767fb4b348fd6c6dbcb76c3a5f68f23434f0bd
r0ckwav3 <peterrockwave@gmail.com>
2023-04-04 09:48:10 -0700

    button sprite reflects state (hover, select)

--- src/StoneButton.js
+++ src/StoneButton.js
@@ -1,8 +1,14 @@
+import React, {useState} from 'react';
+
 export function StoneButton2x1({onClick, selected, children}){
+  const [hover, setHover] = useState(false);
+
   const unselectedStyle = {backgroundImage:`url(${require("./images/buttons/2x1Button.png")})`};
-  const style = unselectedStyle;
+  const hoverStyle = {backgroundImage:`url(${require("./images/buttons/2x1ButtonHover.png")})`};
+  const selectedStyle = {backgroundImage:`url(${require("./images/buttons/2x1ButtonSelected.png")})`};
+  const style = selected ? selectedStyle : (hover ? hoverStyle : unselectedStyle);
   return(
-    <div className="stoneButtonBG" style={style} onClick={onClick}>
+    <div className="stoneButtonBG" style={style} onClick={onClick} onMouseOver={()=>setHover(true)} onMouseOut={()=>setHover(false)}>
         {children}
     </div>
   );
